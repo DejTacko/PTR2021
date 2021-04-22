6105e04be1ae5c450e18379936e620ccc2e7cba8
LukaKrasevec <55098460+LukaKraseuc@users.noreply.github.com>
2021-04-22 14:21:16 +0200

    popraulena 1. naloga neki pr 4.

--- naloga1.py
+++ naloga1.py
@@ -1,16 +1,16 @@
 counter = 0
 curr = 0
 high = 0
-
-games = [20, 10, -35, 20, 15, -10, -20, 20, -30, -10]
+i = 0
+games = [10, -20, 10, 10, 20, 20, -35]
 for i in range(0, len(games)):
     curr = curr + games[i]
+    if curr < 0:
+        counter = 0
     if high < counter:
         high = counter
 
     counter = counter + 1
 
-    if curr < 0:
-        counter = 0
 
 print(high)
--- naloga4.py
+++ naloga4.py
@@ -1,6 +1,6 @@
-pyramid = [[3, 2, 3, 4, 5], [4, 5, 6, 6], [3, 5, 5], [5, 6], [8]]
+pyramid = [[4, 6, 8, 8, 3, 8], [8, 3, 4, 7, 6], [3, 4, 9, 6], [2, 6, 5], [3, 5], [8]]
 force = []
-force2 = [0]
+force2 = []
 y = 0
 x = 0
 
@@ -8,14 +8,17 @@ for y in range(len(pyramid[x])):
     for x in range(len(pyramid[y])):
         force.append(0)
         force2.append(0)
-        force[x] = force[x] + pyramid[y][x]
+        force3.append(0)
         if y == 0:
+            force[x] = force[x] + pyramid[y][x]
             force2[x] = force2[x] + pyramid[y][x]
         else:
+            force[x] = force[x] + pyramid[y][x]
             force2[x+y] = force2[x+y] + pyramid[y][x]
 
+
         x += 1
-    print(force2)
+    print(force)
 
     x = 0
     y += 1
